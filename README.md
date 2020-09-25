a509e197638d4494e105c50368e70145d113a1f5
David Randler <david.randler@gmail.com>
2020-09-25 23:44:34 +0200

    add withoutAuth

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # HTTP Auth examples
 
+## Without auth
+
+The file `withoutAuth.js` does not include any authentication.
+It should essentially be the code required to fulfill #4 of the API exercises + a new /employee route.
+
+It's the baseline for the following demos:
+
 ## Cookie + Session
+
+Found in file `cookies.js`. Matching HTML file is `public/cookies.html`.
 
 Start the express server with session support: `npm run cookies`
 
@@ -13,6 +22,8 @@ You should see an item with the Key 'connect.sid'
 This cookie is automatically added to every request to 'http://localhost:3000'.
 
 ## Auth header + Token 
+
+Found in file `tokens.js`. Matching HTML file is `public/tokens.html`.
 
 Start the express server with session support: `npm run tokens`
 
---
+++ withoutAuth.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const crypto = require('crypto');
+ 
+function encrypt(password) {
+  return crypto.pbkdf2Sync(
+    password, 
+    'randomStringYouShouldChange', 
+    100000, 
+    64, 
+    'sha512'
+  ).toString('hex');
+}
+
+const users = [];
+const port = 3000;
+const app = express();
+ 
+app.use(express.json());
+app.use(express.static('public'));
+ 
+app.post('/register', function (req, res) {  
+  if ( !req.body.email 
+    || !req.body.password 
+    || typeof req.body.email !== 'string' 
+    || typeof req.body.email !== 'string'
+  ) {
+    res.sendStatus(400);
+  }
+
+  users.push({ email: req.body.email, passwordDerivative: encrypt(req.body.password) });
+
+  res.status(201).send('registered user with mail: ' + req.body.email);
+})
+
+app.post('/login', function (req, res) {
+  if ( !req.body.email 
+    || !req.body.password 
+    || typeof req.body.email !== 'string' 
+    || typeof req.body.email !== 'string'
+  ) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const encryptedPassword = encrypt(req.body.password);
+
+  if (users.some(u => u.email === req.body.email && u.passwordDerivative === encryptedPassword)) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(401);
+  }
+})
+ 
+app.get('/employees', function (req, res) {
+  console.log(req.session.user);
+  
+  res.json({ data: [
+    {
+      id: "915391",
+      salary: 60000
+    },
+    {
+      id: "234892",
+      salary: 90000
+    }
+  ]});
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+})
